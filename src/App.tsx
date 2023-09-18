97e5b961dfa812b78fb4d576f8bac965af235e17
ruivergani <ruiverganineto@gmail.com>
2023-09-18 22:26:34 +0100

    Creating Global Styles using styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from 'styled-components'; // Import Theme Provider
 import { defaultTheme } from './styles/themes/default'; // Default Theme Created
 
 import { Button } from "./components/Button";
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
@@ -11,6 +12,8 @@ export function App() {
       <Button variant="success"/>
       <Button variant="danger"/>
       <Button/>
+      
+      <GlobalStyle></GlobalStyle>
     </ThemeProvider>
   )
 }

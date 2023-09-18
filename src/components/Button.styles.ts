6b4f630483cfc2aacd4a43f5d074ff156f691aae
ruivergani <ruiverganineto@gmail.com>
2023-09-18 18:19:34 +0100

    Style Components: Learning using TypeScript

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+// Create a type in TypeScript 
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+
+interface ButtonContainerProps{
+  variant: ButtonVariant; // must be created because needs styling for all color types
+}
+
+// Object for colors
+const buttonVariants = {
+  primary: 'purple',
+  secondary: 'yellow',
+  danger: 'red',
+  success: 'green'
+};
+
+// Use <> to get properties
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width:100px;
+  height: 40px;
+  // Execute as function => send all props from ButtonContainer (in this case variant)
+  ${props => {
+    return `background-color: ${buttonVariants[props.variant]}`
+  }}
+`;

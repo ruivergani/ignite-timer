2438a52c7064e87f3d945095995a8cbaeb6ccb15
ruivergani <ruiverganineto@gmail.com>
2023-09-23 15:14:15 +0100

    Pages and routes - React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { ThemeProvider } from 'styled-components' // Import Theme Provider
 import { defaultTheme } from './styles/themes/default' // Default Theme Created
-
-import { Button } from './components/Button'
 import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
-      <GlobalStyle></GlobalStyle>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+        <GlobalStyle></GlobalStyle>
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }

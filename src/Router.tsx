cbface34e07972b445046aa2b99b488a87180630
ruivergani <ruiverganineto@gmail.com>
2023-09-23 16:59:22 +0100

    Pages and Route - Layout routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
 import {Routes, Route } from 'react-router-dom'; // lib to create routes
 import { Home } from './pages/Home';
 import { History } from './pages/History';
+import { DefaultLayout } from './layouts/DefaultLayout';
 
 export function Router(){
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout/>}> {/* O LayoutDefault engloba todas as rotas */}
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route> 
     </Routes>
   )
 }

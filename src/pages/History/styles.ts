738f9add0f34b435c26d328cb0f4798ef75437e5
ruivergani <ruiverganineto@gmail.com>
2023-09-24 16:54:38 +0100

    Page History HTML structure and styling

---
+++ src/pages/History/styles.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export const HistoryContainer = styled.main`
+  flex: 1;
+  padding: 3.5rem;
+  display: flex;
+  flex-direction: column;
+  h1{
+    color: ${(props) => props.theme['gray-100']};
+    font-size: 1.5rem;
+    font-weight: 700;
+    line-height: 160%; 
+  }
+`;
+export const HistoryList = styled.div`
+  margin: 2rem 0px;
+  flex: 1;
+  overflow: auto;
+  table{
+    width: 100%;
+    border-collapse: collapse;
+    min-width: 600px;
+    th{
+      background-color: ${(props) => props.theme['gray-600']};
+      padding: 1rem;
+      text-align: left;
+      color: ${(props) => props.theme['gray-100']};
+      font-size: 0.875rem;
+      line-height: 1.6;
+      &:first-child{
+        border-top-left-radius: 8px;
+        padding-left: 1.5rem;
+      }
+      &:last-child{
+        border-top-right-radius: 8px;
+        padding-right: 1.5rem;
+      }
+    }
+    td{
+      background-color: ${(props) => props.theme['gray-700']};
+      border-top: 4px solid ${(props) => props.theme['gray-800']};
+      padding: 1rem;
+      line-height: 1.6;
+      font-size: 0.875rem;
+      &:first-child{
+        padding-left: 1.5rem;
+        width: 50%;
+      }
+      &:last-child{
+        padding-right: 1.5rem;
+      }
+    }
+
+  }
+`;

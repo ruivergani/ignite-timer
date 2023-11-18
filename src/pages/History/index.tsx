29fd29c0121e0629db7611fed9a6bc5c5e320101
ruivergani <ruiverganineto@gmail.com>
2023-11-18 16:32:48 +0000

    date fns format history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { HistoryContainer, HistoryList, Status } from "./styles";
 import { CyclesContext } from '../../contexts/CyclesContext';
+import { formatDistanceToNow} from 'date-fns';
 
 export function History() {
   const { cycles } = useContext(CyclesContext)
@@ -23,7 +24,9 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutes</td>
-                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>{formatDistanceToNow(cycle.startDate, {
+                    addSuffix: true,
+                  })}</td>
                   <td>
                     { cycle.finishedDate && <Status statusColor="green">Done</Status>}
                     { cycle.interruptedDate && <Status statusColor="red">Interrupted</Status>}
